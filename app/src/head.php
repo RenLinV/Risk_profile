63657d4ef7fbac552ce16ea6352a3df5694d9381
RenLinV <124538378+RenLinV@users.noreply.github.com>
2024-06-06 06:15:32 +0300

    Delete app/src directory

--- app/src/head.php
+++
@@ -1,6 +0,0 @@
-<head>
-    <meta chrset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Risk profile servey</title>
-    <link rel="stylesheet" href="app/src/app.css">
-</head>
